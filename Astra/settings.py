c8317779669f7c3a4176d2118562bd8566081d02
ansari-khursaid7tr <61353905+ansari-khursaid7tr@users.noreply.github.com>
2020-11-15 13:16:56 +0545

    Update settings.py

--- Astra/settings.py
+++ Astra/settings.py
@@ -139,7 +139,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'ansari.khursaid7tr@gmail.com'
+EMAIL_HOST_USER = 'your_email'
 EMAIL_HOST_PASSWORD = ''
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
